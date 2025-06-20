6004855db9a3960b7668d87d4b0a0b28ffc0dc2f
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-20 16:58:07 +0530

    Define Methods

--- src/main/java/com/sasmitha/todo/service/ToDoService.java
+++ src/main/java/com/sasmitha/todo/service/ToDoService.java
@@ -1,4 +1,14 @@
 package com.sasmitha.todo.service;
 
+import com.sasmitha.todo.dto.ToDo;
+
+import java.util.List;
+
 public interface ToDoService {
+    boolean persist(ToDo toDo);
+    ToDo searchById(Long id);
+    ToDo searchByName(String name);
+    boolean update(ToDo toDo);
+    boolean delete(Long id);
+    List<ToDo> getAll();
 }
--- src/main/java/com/sasmitha/todo/service/impl/ToDoServiceImpl.java
+++ src/main/java/com/sasmitha/todo/service/impl/ToDoServiceImpl.java
@@ -1,4 +1,38 @@
 package com.sasmitha.todo.service.impl;
 
-public class ToDoServiceImpl {
+import com.sasmitha.todo.dto.ToDo;
+import com.sasmitha.todo.service.ToDoService;
+
+import java.util.List;
+
+public class ToDoServiceImpl implements ToDoService {
+    @Override
+    public boolean persist(ToDo toDo) {
+        return false;
+    }
+
+    @Override
+    public ToDo searchById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ToDo searchByName(String name) {
+        return null;
+    }
+
+    @Override
+    public boolean update(ToDo toDo) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<ToDo> getAll() {
+        return List.of();
+    }
 }
